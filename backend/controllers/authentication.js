f8ea8adc4194ee4cb1122aa005b77c2182d5cf76
TahaHussain50 <professortaha2004@gmail.com>
2025-12-21 23:56:51 +0500

    Update authentication.js

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -47,8 +47,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(201).json(user);
@@ -74,8 +74,8 @@ export const logIn = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 24 * 60 * 60 * 1000,
-      secure: false,
-      sameSite: "strict",
+      secure: true,
+      sameSite: "none",
     });
 
     return res.status(201).json(user);
