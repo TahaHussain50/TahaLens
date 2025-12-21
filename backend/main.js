679f4517104ec23f08d853607904007da31309b5
TahaHussain50 <professortaha2004@gmail.com>
2025-12-22 04:03:07 +0500

    Update main.js

--- backend/main.js
+++ backend/main.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "taha-lens.vercel.app",
+    origin: "https://taha-lens.vercel.app",
     credentials: true,
   })
 );
