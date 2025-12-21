4d6302961b92c5a87031d8dba5d8a59b2736e0bc
TahaHussain50 <professortaha2004@gmail.com>
2025-12-22 04:23:03 +0500

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL);
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000,
+    });
     console.log("Database Connected...");
   } catch (error) {
     console.log("Database Error Not Connected!!!");
