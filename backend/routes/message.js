b90412ffa5d897c826c11530bf5da66c303d9691
TahaHussain50 <professortaha2004@gmail.com>
2026-01-15 20:40:10 +0500

    Update message.js

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,5 +1,6 @@
 import express from "express";
 import checkLogin from "../middlewares/checkLogin.js";
+import dbConnect from "../middlewares/dbConnect.js";
 import { upload } from "../middlewares/multer.js";
 import {
   getAllMessages,
@@ -11,11 +12,24 @@ const messageRouter = express.Router();
 
 messageRouter.post(
   "/send/:receiverId",
+  dbConnect,
   checkLogin,
   upload.single("image"),
   sendMessage
 );
-messageRouter.get("/getAll/:receiverId", checkLogin, getAllMessages);
-messageRouter.get("/prevChats", checkLogin, getPrevUserChats);
+
+messageRouter.get(
+  "/getAll/:receiverId",
+  dbConnect,
+  checkLogin,
+  getAllMessages
+);
+
+messageRouter.get(
+  "/prevChats",
+  dbConnect,
+  checkLogin,
+  getPrevUserChats
+);
 
 export default messageRouter;
