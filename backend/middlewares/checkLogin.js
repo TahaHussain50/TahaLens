fbbaad265d1d572c8ec4dd99b6306a02adf1fa64
TahaHussain50 <professortaha2004@gmail.com>
2026-01-15 20:23:23 +0500

    Update checkLogin.js

--- backend/middlewares/checkLogin.js
+++ backend/middlewares/checkLogin.js
@@ -1,16 +1,18 @@
 import jwt from "jsonwebtoken";
+
 const checkLogin = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(400).json({ message: "Token Is Not Found" });
+      return res.status(401).json({ message: "Unauthorized" });
     }
+
     const verifyToken = jwt.verify(token, process.env.JWT_SECRET);
     req.userId = verifyToken.userId;
-
     next();
   } catch (error) {
-    return res.status(500).json({ message: `Is Login Error ${error}` });
+    return res.status(401).json({ message: "Invalid Token" });
   }
 };
+
 export default checkLogin;
